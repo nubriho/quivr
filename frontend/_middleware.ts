8383f0700b130c9a332ee79ce4c871cbfc25f181
masapasa <apyakurel@gmail.com>
2023-06-10 20:24:30 +0200

    middleware

--- frontend/_middleware.ts
+++ frontend/_middleware.ts
@@ -3,7 +3,6 @@ import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
 // import type { Database } from '@/lib/database.types'
-
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareSupabaseClient({ req, res })
